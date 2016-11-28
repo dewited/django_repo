7f3aac70f1bca0a7b06fa96cc1a2922a0f4b53e6
dewited <wightdonald@gmil.com>
2016-11-27 20:41:04 -0800

    Initial Commit from November 28th

---
+++ law/law/urls.py
@@ -0,0 +1,5 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = [url(r'^admin/', include(admin.site.urls)), url(r'^polls/', include(admin.site.urls))]
+
